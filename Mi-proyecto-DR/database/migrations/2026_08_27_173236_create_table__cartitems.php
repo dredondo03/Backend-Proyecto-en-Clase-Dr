4a66e4f6a353d1137c830bae87b821e25b26cb04
Daniel Fernando Redondo Uribe <dredondo@unab.edu.co>
2026-08-27 12:35:08 -0500

    Migration productos users cart itemss

--- Mi-proyecto-DR/database/migrations/2026_08_27_173236_create_table__cartitems.php
+++ Mi-proyecto-DR/database/migrations/2026_08_27_173236_create_table__cartitems.php
@@ -11,12 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('productos', function (Blueprint $table) {
+        Schema::create('cartitems', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->text('description')->nullable();
-            $table->decimal('price', 8, 2);
             $table->timestamps();
+            $table->integer('quantity');
+
         });
     }
 
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('productos');
+        Schema::dropIfExists('cartitems');
     }
 };

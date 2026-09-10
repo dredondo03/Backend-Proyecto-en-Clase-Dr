bad8154f483b449cd66cbd4989439399d36574f4
Daniel Fernando Redondo Uribe <dredondo@unab.edu.co>
2026-09-10 12:02:23 -0500

    Factory Category

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    use HasFactory; /*esto es un import para hacer el factory */
     protected $table = 'categories';
     protected $primaryKey = 'id';
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,10 +17,20 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
+/*ESTA PARTE DEL USER SE DEJA EN EL PRIME SAVE, LUEGO SE COMENTA PARA PODER AGREGAR MAS  */
+      /*  User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ]);*/
+/*llama los modelos, verificar que haga la importacion */
+        $category1 = new Category();
+        $category1->name = 'Electronics';
+        $category1->description = 'Electronic devices and accessories';
+
+        /*esto guarda en la base de datos */
+        /* php artisan db:seed   (guarda todo)*/
+        $category1->save();
+
+        Category::factory(1000)->create(); /*esto es para crear 1000 categorias de manera automatica */
     }
 }
